b3fcfbffe4ba47d7873425c8929ac29afc315590
Ramila <milamukhi@gmail.com>
2018-12-03 13:07:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,5 +14,16 @@ function sayhello(){
 
 
 }
+function saypressthis(){
+	alert("HEY")
+	var feeling= prompt("How are you?");
+
+	if(feeling=="good,bad,okay"){
+		alert("pressthis "+ feeling);
+	}
+	else{
+		alert("that's not how I feel");
+	}
 
 
+}
