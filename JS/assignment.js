8523e1d790597b11d976bea7e340b895fc30fc7d
Ramila <milamukhi@gmail.com>
2019-01-23 12:18:55 -0500

    Create assignment.js

---
+++ JS/assignment.js
@@ -0,0 +1,26 @@
+
+for(int i = 1; i <= 100; i++) {
+
+    if (i % 3 ==0 && i % 5 == 0)
+System.out.printIn(“FizzBuzz”);
+}
+else if(i % 3 ==0) {
+System.out.printIn(“Fizz”);
+}
+else if(i % 5 ==0){
+System.out.printIn(“Buzz”);
+}
+else {
+System.out.printIn(i);
+} 
+
+public static void main(String){
+}
+for(int i=1; i<=100;i++){
+}
+	for(int j=1; j=4;j++){
+		System.out.printIn(j + " ")
+	}
+
+
+
